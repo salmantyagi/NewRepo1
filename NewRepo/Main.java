31b88421146833354d0575311df2447587671e07
Salman Khan <Salman Khan>
2016-09-19 15:54:17 +0530

    a new line of code is added from local repository in Main.java

--- NewRepo/Main.java
+++ NewRepo/Main.java
@@ -5,5 +5,6 @@ class Main
 		System.out.println("Hello World!");
 		System.out.println("hi i am salman tyagi from remote repository");
 		System.out.println("hello i am speaking from the changes in  remote repository")
+		System.out.println("hello i am speaking from the changes in local repository");
 	}
 }
